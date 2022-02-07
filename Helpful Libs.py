c406e06c112b70c1f297087149e8e033514c26b5
Malachi-King <sellingeri@hotmail.com>
2022-02-06 23:26:49 -0600

    Create Helpful Libs.py

---
+++ Helpful Libs.py
@@ -0,0 +1,23 @@
+
+
+from tkinter import (messagebox, Tk, Entry, mainloop)
+import tkinter as tk
+
+def tkinterUIGeneration1():
+    master = Tk()
+    ##master.geometry("500x200")
+    userInput1 = tk.StringVar()
+    prompt1 = tk.Label(master, text = "Give me an animal: " ).pack()
+    entry1 = tk.Entry(master, textvariable = userInput1).pack()
+    button1 = tk.Button(master, text = "Confirm...", command-tkinterUIGeneration2).pack()
+
+def tkinterUIGeneration2():
+    print("penis")
+
+def main():
+    tkinterUIGeneration1()
+
+if __name__ == "__main__":
+    main()
+
+mainloop()
